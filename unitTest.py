f97e021e87f3c5ad36e431febb96fbb7d084a406
Apoorva Choudhary <apoorva@Apoorvas-MacBook-Pro.local>
2020-04-25 18:24:18 +0530

    remove xmlrunner

--- unitTest.py
+++ unitTest.py
@@ -29,6 +29,4 @@ class calculatorTest(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    import xmlrunner
-    runner = xmlrunner.XMLTestRunner(output='test-reports')
-    unittest.main(testRunner=runner)
+    unittest.main()
