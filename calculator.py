8ecf5f7668e73137862bfd6192be868ebf0e339c
Apoorva Choudhary <choudhary.apoorva385@gmail.com>
2020-05-08 19:11:08 +0530

    final

--- calculator.py
+++ calculator.py
@@ -6,49 +6,33 @@ class Calculator:
         self.ans = None
 
     def add(self):
+        print("Addition of: ",self.a, "and ",self.b)
         self.ans = self.a + self.b
         return self.ans
 
     def mul(self):
+        print("Multiplication of:",self.a, "and ",self.b)
+
         self.ans = self.a*self.b
         return self.ans
 
     def div(self):
+        print("Division of:" ,self.a ,"and " ,self.b)
         self.ans = round(self.a/self.b,2)
         return self.ans
 
     def sub(self):
+        print("Subtraction of:" ,self.a ,"from " ,self.b)
         self.ans = self.a-self.b
         return self.ans
 
 
 if __name__ == '__main__':
 
-    a = int(input("Enter first number: "))
-    b = int(input("Enter second number: "))
+    obj = Calculator(3,4)
+    print(obj.add(),"\n")
+    print(obj.sub(),"\n")
+    print(obj.mul(),"\n")
+    print(obj.div(),"\n")
 
-    obj = Calculator(a,b)
-    choice =1
-
-    while choice!=0:
-
-        print("Please select operation -\n" \
-                "0. Exit\n" \
-                "1. Add\n" \
-                "2. Subtract\n" \
-                "3. Multiply\n" \
-                "4. Divide\n")
-
-        choice=int(input("Enter choice: "))
-        if choice==1:
-            print("Result: ",obj.add())
-        elif choice==2:
-            print("Result: ",obj.sub())
-        elif choice==3:
-            print("Result: ",obj.mul())
-        elif choice==4:
-            print("Result: ",obj.div())
-        elif choice==0:
-            print("Exiting!")
-        else:
-            print("Invalid choice!!")
+    
